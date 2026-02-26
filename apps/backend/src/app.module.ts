c8109b5e7aa07a2ab8e5db4875ac63c94ed4fa77
telare <andrewzhubinskiy21@gmail.com>
2026-02-26 19:51:15 +0200

    feat(modules/ai): implement core logic chore(package.json): add config package in deps feat(app.module.ts): implement logic for global config module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,9 +5,18 @@ import { FailuresModule } from './modules/failures/failures.module';
 import { AiModule } from './modules/ai/ai.module';
 import { LogicEngineModule } from './modules/logic-engine/logic-engine.module';
 import { ValidatorModule } from './modules/validator/validator.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FailuresModule, AiModule, LogicEngineModule, ValidatorModule],
+  imports: [
+    FailuresModule,
+    AiModule,
+    LogicEngineModule,
+    ValidatorModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
